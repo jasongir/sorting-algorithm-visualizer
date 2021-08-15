ffe6762bc7244992cd54d2bc1ef2d4e405a8fff9
Jason <snujugroix@gmail.com>
2021-08-15 14:53:37 -0400

    beautified the home page a lil

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
+import Typography from "@material-ui/core/Typography";
+
 import "./home.styles.css";
 
 const Home = () => {
 	return (
 		<div className="home-page">
-			<h1>The Sorting Algorithm Visualizer!</h1>
-			then have a cool home screen with screen shots from each of the sorts or
-			something
+			<Typography variant="h4" component="h1">
+				The Sorting Algorithm Visualizer!
+			</Typography>
+			<Typography variant="body2" component="p">
+				I promise this page is under construction... try exploring other pages
+				using the top-left menu :)
+			</Typography>
 		</div>
 	);
 };
