4aeb7f8286d516103442f7229dead9f38b2eb156
Jason <snujugroix@gmail.com>
2021-08-15 14:56:07 -0400

    just added some padding to home page

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -7,13 +7,15 @@ import "./home.styles.css";
 const Home = () => {
 	return (
 		<div className="home-page">
-			<Typography variant="h4" component="h1">
-				The Sorting Algorithm Visualizer!
-			</Typography>
-			<Typography variant="body2" component="p">
-				I promise this page is under construction... try exploring other pages
-				using the top-left menu :)
-			</Typography>
+			<div className="home-inner-contaie">
+				<Typography variant="h4" component="h1">
+					The Sorting Algorithm Visualizer!
+				</Typography>
+				<Typography variant="body2" component="p">
+					I promise this page is under construction... try exploring other pages
+					using the top-left menu :)
+				</Typography>
+			</div>
 		</div>
 	);
 };
